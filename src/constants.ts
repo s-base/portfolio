1a14613a8ae5cdb57a575ab4a59f97000941c2a5
willibaur <william.bautista-romero@smartpension.co.uk>
2024-02-05 18:53:31 +0000

    Use constants

---
+++ src/constants.ts
@@ -0,0 +1,17 @@
+interface Social {
+  name: string;
+  url: string;
+}
+
+export const socials: { [key: string]: Social } = {
+  instagram: {
+    name: 'Instagram',
+    url: 'https://www.instagram.com/sofiabs/',
+  },
+  linkedin: {
+    name: 'Linkedin',
+    url: 'https://www.linkedin.com/sofiabs/',
+  }
+}
+
+export const username: string = 'Sofia B-S'
