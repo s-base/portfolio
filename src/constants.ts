b71f9d534fa336a4ce3a5df66444ee00b0f9c424
willibaur <williambautista@gmail.com>
2024-02-15 18:18:55 +0000

    Adjust repo for deployment

--- src/constants.ts
+++ src/constants.ts
@@ -13,5 +13,13 @@ export const socials: { [key: string]: Social } = {
     url: 'https://www.linkedin.com/sofiabs/',
   }
 }
-
+export const base: string = '/portfolio'
+export const assets: string = `${base}/assets`
+export const email: string = 's.bautista.sepulveda+portfolio@gmail.com'
+export const urls: { [key: string]: string } = {
+  404: `${base}/404/`,
+  about: `${base}/about/`,
+  home: `${base}/`,
+  work: `${base}/work/`,
+}
 export const username: string = 'Sofia'
--- src/content/work/sketchbook/sketchbook-one.md
+++ src/content/work/sketchbook/sketchbook-one.md
@@ -2,7 +2,7 @@
 featured: true
 title: Sketchbook test
 publishDate: 2020-03-02 00:00:00
-img: /assets/stock-1.jpg
+img: /portfolio/assets/stock-1.jpg
 img_alt: Iridescent ripples of a bright blue and pink liquid
 description: |
   We designed a whodunnit-style game to introduce Markdown formatting. Suspense — suspicion — syntax!
